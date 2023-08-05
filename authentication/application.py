202cf602b5b068ae6e27717ed1eabae0d5140752
Aleksa <aleksad09@gmail.com>
2023-08-05 19:44:39 +0200

    Implement user delete

--- authentication/application.py
+++ authentication/application.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, Response, jsonify
 from configuration import Configuration
 from models import database, User, UserRole
+from sqlalchemy import and_
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, decode_token, create_refresh_token, get_jwt
 import re
 
 EMAIL_REGEX = re.compile(r"^[a-z0-9\.]+@([a-z0-9]+\.)+[a-z]{2,4}$")
@@ -67,6 +69,86 @@ def register(my_request, role_id):
     return Response(status=200)
 
 
+jwt = JWTManager(application)
+
+
+@application.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", "")
+    password = request.json.get("password", "")
+
+    if email == "":
+        response = {"message": "Field email is missing."}
+        return response, 400
+    if password == "":
+        response = {"message": "Field password is missing."}
+        return response, 400
+
+    if not EMAIL_REGEX.match(email):
+        response = {"message": "Invalid email."}
+        return response, 400
+
+    user = User.query.filter(and_(User.email == email, User.password == password)).first()
+
+    if not user:
+        response = {"message": "Invalid credentials."}
+        return response, 400
+
+    additional_claims = {
+        "forename": user.forename,
+        "surname": user.surname,
+        "roles": [str(role) for role in user.roles]
+    }
+
+    access_token = create_access_token(identity=user.email, additional_claims=additional_claims)
+    refresh_token = create_refresh_token(identity=user.email, additional_claims=additional_claims)
+
+    response = {"accessToken": access_token}
+
+    return jsonify(accessToken=access_token, refreshToken=refresh_token), 200
+
+
+@application.route("/check", methods=["POST"])
+@jwt_required()
+def check():
+    return "Token is valid"
+
+
+@application.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    refresh_claims = get_jwt()
+
+    additional_claims = {
+        "forename": refresh_claims["forename"],
+        "surname": refresh_claims["surname"],
+        "roles": refresh_claims["roles"]
+    }
+
+    return Response(create_access_token(identity=identity, additional_claims=additional_claims), status=200)
+
+
+@application.route("/delete", methods=["POST"])
+@jwt_required()
+def delete_user():
+    # token = decode_token(request.headers["Authorization"].split(" ")[1])
+    # email = token["sub"]
+
+    email = get_jwt_identity()
+    user = User.query.filter(User.email == email).first()
+
+    if not user:
+        response = {"message": "Unknown user."}
+        return response, 400
+
+    UserRole.query.filter(UserRole.user_id == user.id).delete()
+    User.query.filter(User.id == user.id).delete()
+    database.session.commit()
+
+    return Response(status=200)
+
+
 if __name__ == "__main__":
     database.init_app(application)
-    application.run(debug=True, host="0.0.0.0", port=5002)
+    application.run(debug=True, host="0.0.0.0", port=5000)
--- authentication/migrate.py
+++ authentication/migrate.py
@@ -15,25 +15,26 @@ if not database_exists(application.config["SQLALCHEMY_DATABASE_URI"]):
 database.init_app(application)
 
 with application.app_context() as context:
+    database.init_app(application)
     init()
     migrate(message="Production migration")
     upgrade()
 
-    admin_role = Role(name="admin")
+    owner_role = Role(name="owner")
     customer_role = Role(name="customer")
     courier_role = Role(name="courier")
 
-    database.session.add(admin_role)
+    database.session.add(owner_role)
     database.session.add(customer_role)
     database.session.add(courier_role)
     database.session.commit()
 
-    admin = User(email="admin@admin.com", password="123", forename="admin", surname="admin")
+    owner = User(email="onlymoney@gmail.com", password="evenmoremoney", forename="Scrooge", surname="McDuck")
 
-    database.session.add(admin)
+    database.session.add(owner)
     database.session.commit()
 
-    user_role = UserRole(user_id=admin.id, role_id=admin_role.id)
+    user_role_owner = UserRole(user_id=owner.id, role_id=owner_role.id)
 
-    database.session.add(user_role)
+    database.session.add(user_role_owner)
     database.session.commit()
